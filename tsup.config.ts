833f81cbf7c30d4b4fc2a813f93a4144bca9da23
chiba <qwq@qwwq.org>
2026-04-08 17:37:48 +0800

    修改打包方式

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,10 +3,11 @@ import { defineConfig } from "tsup";
 const coverageSourcemapEnabled = process.env.YUME_DSL_COVERAGE_SOURCEMAP === "1";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
+  entry: ["src/*.ts"],
   format: ["esm", "cjs"],
   dts: true,
   clean: true,
+  bundle: false,
   sourcemap: coverageSourcemapEnabled,
   splitting: false,
 });
