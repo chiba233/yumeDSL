b9ef08281ffda331848a2bbcfad3767b7dd6bac0
chiba <qwq@qwwq.org>
2026-04-09 10:35:53 +0800

    fix:修复新的打包方式会把18扬了的问题

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,11 +3,10 @@ import { defineConfig } from "tsup";
 const coverageSourcemapEnabled = process.env.YUME_DSL_COVERAGE_SOURCEMAP === "1";
 
 export default defineConfig({
-  entry: ["src/*.ts"],
+  entry: ["src/index.ts"],
   format: ["esm", "cjs"],
   dts: true,
   clean: true,
-  bundle: false,
   sourcemap: coverageSourcemapEnabled,
   splitting: false,
 });
